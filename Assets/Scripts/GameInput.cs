e7a6b20139c29d4e4d913904b7852c298aed1a5f
Dilara Bakır <dilaraa.bakir@gmail.com>
2025-08-26 22:58:02 +0300

    added cutting, progress bar, trash counter and stove counter

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -22,7 +22,7 @@ public class GameInput : MonoBehaviour {
         OnInteractAlternateAction?.Invoke(this, EventArgs.Empty);
     }
 
-    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj) {
+    private void Interact_performed(InputAction.CallbackContext obj) {
         OnInteractAction?.Invoke(this, EventArgs.Empty);
 
     }
