2bd8cdd0c02756c30486b7713d892a55204b8beb
Dilara Bakır <dilaraa.bakir@gmail.com>
2025-09-17 17:13:33 +0300

    added controller input & menu navigation

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -15,6 +15,9 @@ public class GameInput : MonoBehaviour {
         Interact,
         InteractAlternate,
         Pause,
+        Gamepad_Interact,
+        Gamepad_InteractAlternate,
+        Gamepad_Pause,
     }
 
     public event EventHandler OnInteractAction;
@@ -88,6 +91,12 @@ public class GameInput : MonoBehaviour {
                 return playerInputActions.Player.InteractAlternate.bindings[0].ToDisplayString();
             case Binding.Pause:
                 return playerInputActions.Player.Pause.bindings[0].ToDisplayString();
+            case Binding.Gamepad_Interact:
+                return playerInputActions.Player.Interact.bindings[1].ToDisplayString();
+            case Binding.Gamepad_InteractAlternate:
+                return playerInputActions.Player.InteractAlternate.bindings[1].ToDisplayString();
+            case Binding.Gamepad_Pause:
+                return playerInputActions.Player.Pause.bindings[1].ToDisplayString();
 
         }
     }
@@ -128,6 +137,19 @@ public class GameInput : MonoBehaviour {
                 inputAction = playerInputActions.Player.Pause;
                 bindingIndex = 0;
                 break;
+            case Binding.Gamepad_Interact:
+                inputAction = playerInputActions.Player.Interact;
+                bindingIndex = 1;
+                break;
+            case Binding.Gamepad_InteractAlternate:
+                inputAction = playerInputActions.Player.InteractAlternate;
+                bindingIndex = 1;
+                break;
+            case Binding.Gamepad_Pause:
+                inputAction = playerInputActions.Player.Pause;
+                bindingIndex = 1;
+                break;
+
         }
 
         inputAction.PerformInteractiveRebinding(bindingIndex)
