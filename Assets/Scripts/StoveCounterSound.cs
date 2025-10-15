ad2d9ced522943620e277f247379dcf5eff81bf1
Dilara Bakır <dilaraa.bakir@gmail.com>
2025-10-15 23:57:36 +0300

    polish

--- Assets/Scripts/StoveCounterSound.cs
+++ Assets/Scripts/StoveCounterSound.cs
@@ -6,6 +6,8 @@ public class StoveCounterSound : MonoBehaviour {
 
 
     private AudioSource audioSource;
+    private float warningSoundTimer;
+    private bool playWarningSound;
 
     private void Awake() {
         audioSource = GetComponent<AudioSource>();
@@ -13,6 +15,12 @@ public class StoveCounterSound : MonoBehaviour {
 
     private void Start() {
         stoveCounter.OnStateChanged += StoveCounter_OnStateChanged;
+        stoveCounter.OnProgressChanged += StoveCounter_OnProgressChanged;
+    }
+
+    private void StoveCounter_OnProgressChanged(object sender, IHasProgress.OnProgressChangedEventArgs e) {
+        float burnShowProgressAmount = .5f;
+        playWarningSound = stoveCounter.isFried() && e.progressNormalized >= burnShowProgressAmount;
     }
 
     private void StoveCounter_OnStateChanged(object sender, StoveCounter.OnStateChangedEventArgs e) {
@@ -22,6 +30,18 @@ public class StoveCounterSound : MonoBehaviour {
         }
         else {
             audioSource.Pause();
+        }
+    }
+
+    private void Update() {
+        if (playWarningSound) {
+            warningSoundTimer -= Time.deltaTime;
+            if (warningSoundTimer < 0f) {
+                float warningSoundTimerMax = .2f;
+                warningSoundTimer = warningSoundTimerMax;
+
+                SoundManager.Instance.PlayWarningSound(stoveCounter.transform.position);
+            }
         }
     }
 }
